0a6388fbc54a34a506b56e76f4002bfededc6e65
Syed Nabeel <megadev85@gmail.com>
2024-05-04 15:27:38 +0500

    initial commit

---
+++ src/main/java/org/example/employee_leave_management/EmployeeLeaveManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.employee_leave_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLeaveManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/employee_leave_management/EmployeeLeaveManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.employee_leave_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLeaveManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
